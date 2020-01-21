6fcfa44a144f187d2544454a54b66baa0f833be0
Patrick McCorry <stonecoldpat@gmail.com>
2020-01-21 22:09:35 +0000

    removed comment

--- src/contracts/BroadcasterWithOrdering.sol
+++ src/contracts/BroadcasterWithOrdering.sol
@@ -11,9 +11,6 @@ contract BroadcasterWithOrdering is BitFlipMetaTransactionWithOrdering {
 
         // To support ERC712, we can remove signature from replay protection
         // and include it as a field in ERC712.
-        // It is included in the meta-transaction sigature.
-        // So it is independent of the replay protection.
-        // bytes32 digest = this.getDigest(abi.encode(PUBLIC_BROADCAST_TYPEHASH, _message, _sender, _index, _toFlip));
         isMetaTransactionApproved(keccak256(abi.encode(_message)), _sender, _index, _toFlip, _sig);
 
         emit PublicBroadcast(_message);
